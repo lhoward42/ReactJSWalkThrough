247d038f56bb847051c3b497f7e2bfbb97ee34dc
Laurice Howard <lauricehoward@Laurices-MacBook-Air-2.local>
2021-06-21 15:43:45 -0400

    React JS Walkthough complete through Module 2.4

---
+++ src/components/concepts/FunctionalComponents/FunctionalComponentsDemo.js
@@ -0,0 +1,23 @@
+const FunctionalComponentDemo = () => {
+    return(
+        <div className='main'>
+            <div className='mainDiv'>
+                <h1>Functional Component</h1>
+                <p>Functional Components are the primary tool in React to build a small, modular piece of your page.</p>
+                <dl>
+                    <dt>Can use state</dt>
+                    <dd>With the 'useState' hook, functional components can now both render a display to the page and update the information to be shown.</dd>
+                    <dt>No 'this' keyword</dt>
+                    <dd>Older class components use 'this', functional components have no 'this' object.</dd>
+                    <dt>Can use effects</dt>
+                    <dd>With the 'useEffect' hook, functional</dd>
+                    <dt>return()</dt>
+                    <dd>Must return a single element, but this element may have elements inside.</dd>
+                </dl>
+                <h1>Functional Syntax versus Arrow Function</h1>
+            </div>
+        </div>
+    )
+}
+
+export default FunctionalComponentDemo
